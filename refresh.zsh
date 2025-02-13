77fc3ad063528781e66c0463a797c192b4339e1a
Ben Tian <ben.tian@gmail.com>
2025-02-13 08:12:23 +0800

    Add 2M experiment results of ds_factor=4 for comparison.

--- refresh.zsh
+++ refresh.zsh
@@ -11,17 +11,22 @@ output_list=()
 # Iterate over each key-value pair in the dictionary
 for key value in ${(kv)my_dict}; do
   out_dir="docs/experiments"
-  nomask="${key}_nomask_2M"
-  masked="${key}_2M"
+
+  # Experiments
+  nomask_2M="${key}_nomask_2M"
+  masked_2M="${key}_2M"
   masked_4M="${key}_4M"
+  masked_2M_ds4="${key}_2M_ds4"
 
-  python3 corrdiff_plotgen.py "$value/2M" "$out_dir/$nomask" --masked="no" --max-duration=2000000
-  python3 corrdiff_plotgen.py "$value/2M" "$out_dir/$masked" --max-duration=2000000
+  python3 corrdiff_plotgen.py "$value/2M" "$out_dir/$nomask_2M" --masked="no"
+  python3 corrdiff_plotgen.py "$value/2M" "$out_dir/$masked_2M"
   python3 corrdiff_plotgen.py "$value/4M" "$out_dir/$masked_4M"
+  python3 corrdiff_plotgen.py "$value/ds_4/2M" "$out_dir/$masked_2M_ds4"
 
-  output_list+=("$nomask")
-  output_list+=("$masked")
+  output_list+=("$nomask_2M")
+  output_list+=("$masked_2M")
   output_list+=("$masked_4M")
+  output_list+=("$masked_2M_ds4")
 done
 
 # Convert the output_list to a JSON array and save to file
