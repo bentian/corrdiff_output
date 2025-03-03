e52bcae5ac9bd516f305055b299830f903de12b3
Ben Tian <ben.tian@gmail.com>
2025-03-03 11:31:55 +0800

    Add plots and tables for 4M_1322 experiments, which trained with 2013-2022 data (10yr).

--- refresh.zsh
+++ refresh.zsh
@@ -16,16 +16,19 @@ for key value in ${(kv)my_dict}; do
   nomask_2M="${key}_nomask_2M"
   masked_2M="${key}_2M"
   masked_4M="${key}_4M"
+  masked_4M_1322="${key}_4M"
   masked_2M_ds4="${key}_2M_ds4"
 
   python3 corrdiff_plotgen.py "$value/2M" "$out_dir/$nomask_2M" --masked="no"
   python3 corrdiff_plotgen.py "$value/2M" "$out_dir/$masked_2M"
   python3 corrdiff_plotgen.py "$value/4M" "$out_dir/$masked_4M"
+  python3 corrdiff_plotgen.py "$value/4M" "$out_dir/$masked_4M_1322"
   python3 corrdiff_plotgen.py "$value/ds_4/2M" "$out_dir/$masked_2M_ds4"
 
   output_list+=("$nomask_2M")
   output_list+=("$masked_2M")
   output_list+=("$masked_4M")
+  output_list+=("$masked_4M_1322")
   output_list+=("$masked_2M_ds4")
 done
 
