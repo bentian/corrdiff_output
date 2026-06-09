5fc5fc02ad5db59c06a56609e4e4d2036882dd36
Ben Tian <ben.tian@gmail.com>
2026-06-09 21:20:05 +0800

    Do not add tab divider for the first tab.

--- docs/js/render.js
+++ docs/js/render.js
@@ -142,7 +142,8 @@ function renderTabs(content, folder, files, id1, id2) {
     Object.entries(files).forEach(([key, value], index) => {
         const tab = document.createElement("div");
         tab.className = "tab";
-        tab.classList.toggle("tab-group-start", key === "prcp");  // Add divider before `pr`
+        // Add divider before `prcp` except the first tab
+        tab.classList.toggle("tab-group-start", key === "prcp" && index !== 0);
         tab.textContent = key;
         tabs.appendChild(tab);
 
